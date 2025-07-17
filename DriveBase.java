b466f3606e1d397d5d1cce41568aef949bc1c3a6
rsneller <146138576+rsneller@users.noreply.github.com>
2025-07-17 16:54:21 -0600

    Update DriveBase.java
    Making it mecanum instead of tank drive

--- DriveBase.java
+++ DriveBase.java
@@ -1,7 +1,11 @@
 package frc.robot;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import frc.robot.Data.PortMap;
+import frc.robot.Devices.NEOSparkMaxMotor;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 
 public class DriveBase {
     // THESE Numbers might need updated
@@ -13,13 +17,29 @@ public class DriveBase {
     private final SparkMax  m_leftFrontDrive = new SparkMax (frontLeftMotorCadId, MotorType.kBrushless);
     private final SparkMax  m_backLeftDrive = new SparkMax (backRightMotorCadId, MotorType.kBrushless);
 
+    // distance of wheels from center in meters
+    Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381); // these are not actually measured
+    Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381); // these are not actually measured
+    Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381); // these are not actually measured
+    Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381); // these are not actually measured
+
+    // Creating my kinematics object using the wheel locations.
+    MecanumDriveKinematics m_kinematics = new MecanumDriveKinematics(
+            m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
     public DriveBase() {
 
     }
+    public void drive(){
+        ChassisSpeeds speeds = new ChassisSpeeds(forward, strafe, rotation);
+        // Convert to wheel speeds
+        MecanumDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(speeds);
+        // Get the individual wheel speeds
+        frontLeft = wheelSpeeds.frontLeftMetersPerSecond;
+        frontRight = wheelSpeeds.frontRightMetersPerSecond;
 
-    public void move() {
-        this.m_backLeftDrive.set(-1);
-        this.m_leftFrontDrive.set(-1);
+        frontLeftMotor.set(frontLeft*-1);
+        frontRightMotor.set(frontRight);
     }
 
     public void goBackwards() {
